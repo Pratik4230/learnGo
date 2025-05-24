840a6f08f64ab77599329c6c562fa1f45ccd6468
Pratik4230 <pratikjadhav1438@gmail.com>
2025-05-24 21:42:16 +0530

    Print

---
+++ print/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 20
+	name := "Pratik"
+	per := 82.202332
+
+	fmt.Printf("Type of Name is %T \n", name)
+	fmt.Printf("age is %d , name is %s  percentage is %.2f \n", age, name, per)
+
+}
